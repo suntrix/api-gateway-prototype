cf4ff6c0e5ed3f36a76137df42a891bd30ebf485
Sebastian Owodziń <sebastian.owodzin@gmail.com>
2022-03-26 13:44:39 +0000

    code cleanup

--- gateway/src/commonMain/kotlin/suntrix/api/gateway/proxies/MovieProxy.kt
+++ gateway/src/commonMain/kotlin/suntrix/api/gateway/proxies/MovieProxy.kt
@@ -6,10 +6,13 @@ import io.ktor.server.resources.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
 import kotlinx.serialization.Serializable
 import suntrix.api.gateway.target.default.ApiClient
 import suntrix.api.gateway.target.omdb.omdb_searchMovie
 import suntrix.api.gateway.target.tmdb.tmdb_searchMovie
+import suntrix.api.gateway.target.omdb.MovieResponse as omdb_MovieResponse
+import suntrix.api.gateway.target.tmdb.MovieResponse as tmdb_MovieResponse
 
 /**
  * Created by Sebastian Owodzin on 06/03/2022
@@ -26,48 +29,65 @@ data class MovieRoute(
 fun Application.movieProxy() {
 
     val apiClient = ApiClient()
+    val omdbApiKey = "ac6ddc16"
+    val tmdbApiKey = "eeb9798510a128cad0ab19d19db2f563"
 
     routing {
         get<MovieRoute> {
-            val omdbMovieDeferred = async { apiClient.omdb_searchMovie(it.title, it.releaseYear, "ac6ddc16") }
-            val tmdbMovieDeferred = async { apiClient.tmdb_searchMovie(it.title, it.releaseYear, "eeb9798510a128cad0ab19d19db2f563") }
-
-            val omdbData = omdbMovieDeferred.await()
-            val tmdbData = tmdbMovieDeferred.await()
-
-            apiClient.close()
-
-            val ratings = omdbData.ratings.map { Response.Rating(it.source, it.value) }.plus(
-                tmdbData.results.firstOrNull { it.title == omdbData.title }?.run {
-                    listOf(Response.Rating("The Movie Database", "$voteAverage/10"))
-                } ?: emptyList()
-            )
+            val (omdbData, tmdbData) = listOf(
+                async {
+                    apiClient.omdb_searchMovie(it.title, it.releaseYear, omdbApiKey)
+                },
+                async {
+                    apiClient.tmdb_searchMovie(it.title, it.releaseYear, tmdbApiKey)
+                }
+            ).awaitAll()
 
             call.respond(
-                Response(
-                    Response.Data(
-                        omdbData.title,
-                        omdbData.year.toInt(),
-                        omdbData.released,
-                        omdbData.runtime.runtimeMinutes() ?: -1,
-                        omdbData.plot,
-                        omdbData.poster,
-                        ratings
-                    ),
-                    if (it.debug) {
-                        Response.OriginData(
-                            omdbData,
-                            tmdbData
-                        )
-                    } else null
+                buildResponse(
+                    omdbData as omdb_MovieResponse,
+                    tmdbData as tmdb_MovieResponse,
+                    it.debug
                 )
             )
         }
     }
 }
 
-private fun String.runtimeMinutes(): Int? =
-    """(\d+) min""".toRegex().find(this)?.destructured?.component1()?.toInt()
+
+private fun buildResponse(
+    omdbData: omdb_MovieResponse,
+    tmdbData: tmdb_MovieResponse,
+    debug: Boolean
+) = Response(
+    Response.Data(
+        omdbData.title,
+        omdbData.year.toInt(),
+        omdbData.released,
+        omdbData.runtime.runtimeMinutes(),
+        omdbData.plot,
+        omdbData.poster,
+        buildRatings(omdbData, tmdbData)
+    ),
+    if (debug) {
+        Response.OriginData(
+            omdbData,
+            tmdbData
+        )
+    } else null
+)
+
+private fun buildRatings(
+    omdbData: omdb_MovieResponse,
+    tmdbData: tmdb_MovieResponse
+): List<Response.Rating> = omdbData.ratings.map { Response.Rating(it.source, it.value) }.plus(
+    tmdbData.results.firstOrNull { it.title == omdbData.title }?.run {
+        listOf(Response.Rating("The Movie Database", "$voteAverage/10"))
+    } ?: emptyList()
+)
+
+private fun String.runtimeMinutes(): Int? = """(\d+) min""".toRegex().find(this)?.destructured?.component1()?.toInt()
+
 
 @Serializable
 data class Response(
@@ -80,7 +100,7 @@ data class Response(
         val title: String,
         val releaseYear: Int,
         val releaseDate: String,
-        val runtime: Int,
+        val runtimeMinutes: Int?,
         val plot: String,
         val posterUrl: String,
         val ratings: List<Rating> = emptyList()
@@ -94,7 +114,7 @@ data class Response(
 
     @Serializable
     data class OriginData(
-        val omdbData: suntrix.api.gateway.target.omdb.MovieResponse,
-        val tmdbData: suntrix.api.gateway.target.tmdb.MovieResponse
+        val omdbData: omdb_MovieResponse,
+        val tmdbData: tmdb_MovieResponse
     )
 }
